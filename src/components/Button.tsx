832e0b493041e48bb603f1182114878d8be660e1
Marija Kuveljic <kuveljic117@gmail.com>
2024-01-17 12:51:43 +0100

    Modify Button Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/system';
+import SearchIcon from '@mui/icons-material/Search';
 
-interface ButtonProps {
+const StyledButton = styled(Button)({
+  margin: '8px',
+  padding: '8px'
+});
+
+interface CustomButtonProps {
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick }) => {
-  return <button onClick={onClick}>Submit</button>;
+const CustomButton: React.FC<CustomButtonProps> = ({ onClick }) => {
+  return (
+    <StyledButton variant="outlined" color="primary" startIcon={<SearchIcon />} onClick={onClick}>
+      Search
+    </StyledButton>
+  );
 };
 
-export default Button;
+export default CustomButton;
