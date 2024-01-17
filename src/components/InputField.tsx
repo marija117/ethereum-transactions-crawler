a6fa347cee9416b22cd78cf2936b99fe6ce2069a
Marija Kuveljic <kuveljic117@gmail.com>
2024-01-17 12:52:55 +0100

    Modify InputField Component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { styled } from '@mui/system';
+import TextField from '@mui/material/TextField';
+
+const StyledTextField = styled(TextField)({
+  margin: '15px',
+});
 
 interface InputFieldProps {
   label: string;
@@ -9,8 +15,7 @@ interface InputFieldProps {
 const InputField: React.FC<InputFieldProps> = ({ label, value, onChange }) => {
   return (
     <div>
-      <label>{label}</label>
-      <input type="text" value={value} onChange={(e) => onChange(e.target.value)} />
+      <StyledTextField id="filled-basic" label={label} variant="standard" value={value} onChange={(e) => onChange(e.target.value)} />
     </div>
   );
 };
