a9380a103f60fd244eb579cae644de1843b82a8a
Marija Kuveljic <kuveljic117@gmail.com>
2024-01-20 21:59:45 +0100

    Minor fix in Home.styles.

--- src/pages/Home/Home.styles.tsx
+++ src/pages/Home/Home.styles.tsx
@@ -1,16 +1,5 @@
 import { styled } from '@mui/system';
 
-export const StyledFormContainer = styled('div')`
-  @media (max-width: 600px) {
-    flex-direction: column;
-  }
-  margin-top: 50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-`;
-
 export const StyledTableContainer = styled('div')({
   marginTop: '100px',
   display: 'flex',
