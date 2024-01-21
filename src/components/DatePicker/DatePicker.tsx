d6147c73cdd1f4e243feded1bda7b82fa79fd739
Marija Kuveljic <kuveljic117@gmail.com>
2024-01-21 22:21:11 +0100

    Implement DatePicker component.

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+interface DateTimePickerProps {
+  label: string;
+  value: Date | null;
+  onChange: (date: Date | null) => void;
+}
+
+const DateTimePicker: React.FC<DateTimePickerProps> = ({ label, value, onChange }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleDateChange = (newDate: Date | null) => {
+    onChange(newDate);
+    setOpen(false);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        label={label}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        value={value}
+        onChange={handleDateChange}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default DateTimePicker;
