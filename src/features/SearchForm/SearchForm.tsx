077c649b6ee0e72385d3b094660b6321ff5f354e
Marija Kuveljic <kuveljic117@gmail.com>
2024-01-21 22:23:43 +0100

    Modify SearchForm so it can be reusable.

--- src/features/SearchForm/SearchForm.tsx
+++ src/features/SearchForm/SearchForm.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import InputField from '../../components/InputField/InputField';
 import CustomButton from '../../components/Button/Button';
+import InputField from '../../components/InputField/InputField';
+import DatePicker from '../../components/DatePicker/DatePicker';
 import { StyledSearchFormContainer } from "./SearchForm.styles";
 
 interface SearchFormProps {
-  'data-testid': string,
+  'data-testid': string;
   walletAddress: string;
-  fromBlock: string;
+  fromBlock?: string;
+  date?: Date | null;
   onWalletAddressChange: (value: string) => void;
-  onFromBlockChange: (value: string) => void;
+  onFromBlockChange?: (value: string) => void;
+  onDateChange?: (date: Date | null) => void;
   onSearchClick: () => void;
 }
 
@@ -16,14 +19,21 @@ const SearchForm: React.FC<SearchFormProps> = ({
   'data-testid': testId,
   walletAddress,
   fromBlock,
+  date,
   onWalletAddressChange,
   onFromBlockChange,
+  onDateChange,
   onSearchClick,
 }) => {
   return (
     <StyledSearchFormContainer data-testid={testId}>
       <InputField id="walletAddress" label="Wallet Address" value={walletAddress} onChange={onWalletAddressChange} />
-      <InputField id="fromBlock" label="From Block" value={fromBlock} onChange={onFromBlockChange} />
+      {fromBlock !== undefined && onFromBlockChange && (
+        <InputField id="fromBlock" label="From Block" value={fromBlock} onChange={onFromBlockChange} />
+      )}
+      {date !== undefined && onDateChange && (
+        <DatePicker label="Select Date" value={date} onChange={onDateChange} />
+      )}
       <CustomButton label="Search" onClick={onSearchClick} />
     </StyledSearchFormContainer>
   );
