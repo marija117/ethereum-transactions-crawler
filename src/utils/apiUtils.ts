d971a0934ee5ac367ed63f8b92979cc3a5011c03
Marija Kuveljic <kuveljic117@gmail.com>
2024-01-22 02:44:41 +0100

    Ipmlement getEthValueAtDate.

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,18 +1,32 @@
-import { Alchemy, Network } from 'alchemy-sdk';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
+import EthDater from 'ethereum-block-by-date';
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
 
-export const fetchData = async (fromBlock: string, walletAddress: string) => {
-  const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    network: Network.ETH_MAINNET,
-  };
-  const alchemy = new Alchemy(config);
-  const categories = ['external', 'internal', 'erc20', 'erc721', 'erc1155'];
+const dater = new EthDater(alchemy.core as any);
+const categories = ['external', 'internal', 'erc20', 'erc721', 'erc1155'];
 
+export const fetchData = async (walletAddress: string, fromBlock: string) => {
   const data = await alchemy.core.getAssetTransfers({
     fromBlock: '0x' + Number(fromBlock).toString(16),
     fromAddress: walletAddress,
+    withMetadata: true,
     category: categories as any,
   });
 
   return data.transfers;
+};
+
+export const getEthValueAtDate = async (walletAddress: string, date: Date | null) => {
+  const timestamp = (date as Date).toISOString();
+  let block = await dater.getDate(timestamp);
+  block  = block['block'] as any
+
+  const balanceResponse = await alchemy.core.getBalance(walletAddress, block as any);
+  const ethBalance = Utils.formatEther(balanceResponse);
+
+  return parseFloat(ethBalance);
 };
